1bbd3bede450be4d9492085c3f32e3bb8e79bd36
cquigley2 <91629921+cquigley2@users.noreply.github.com>
2021-12-06 14:34:26 -0800

    Update test_pv_model.py

--- SolarSizer/tests/test_pv_model.py
+++ SolarSizer/tests/test_pv_model.py
@@ -18,3 +18,14 @@ class TestPVModel(unittest.TestCase):
         """
         pvmodel.execute_pvmodel(5, 6, n_inverters=10)
         return
+      
+# One shot: In this case, you call the code under test with arguments for which you know the expected result
+#   1. We could do this with the above smoke test or create another set up where we know the output
+
+# Edge test: The code under test is invoked with arguments that should cause an exception, and you evaluate if the expected exception occurrs
+#   1. input a load profile that is way too high, still need to handle this case in psyam_model.py
+#   2. Lat,Lon over ocean? 
+
+# Pattern test: Based on your knowledge of the *calculation* (not implementation) of the code under test, you construct a suite of test cases for which the results are known or there are known patterns in these results that are used to evaluate the results returned
+#   1. run the code twice, one load profile is larger than the other, compare outputs 
+#   2. same thing but with locations, choose one location that has much more irradiance 
