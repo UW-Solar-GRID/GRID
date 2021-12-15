e52ca1e8541e5b716fdb6e022f54692f9faa1a3f
Ning <nwang9614@gmail.com>
2021-12-14 19:13:47 -0800

    Made GUI more good looking

--- solarsizer/GUI.py
+++ solarsizer/GUI.py
@@ -16,6 +16,7 @@ import urllib.request
 import os
 import base64
 import io
+import time
 
 from decimal import Decimal
 from pysam import pysam_model
@@ -85,110 +86,158 @@ app.layout = html.Div( children=[
     
     # Main block
     dbc.Row([
-        html.Label('Try'),
+        html.Div([
+            
+        dbc.Row([
+            dbc.Col([
+                html.Div([
+            'What is SolarSizer?' 
+                ],
+                style={
+                    'font-size':24,
+                    'font-weight': 'bold'
+                    }
+            ),
+                html.Div([
+                    'The SolarSizer program assists in the planning of small off-grid solar projects by creating a user-friendly dashboard. Based on the input location and load profile, the model returns an equipment list of a solar array capable of meeting the load profile. SolarSizer applies a model designed by GRID yet with additional optimization and simplicity. The project also added documentation for testing and expanding the original code.'
+                ],
+                style={
+                    'font-size': 20,
+                }
+                ),
                 html.Br(),
-                html.Br(),
-                html.Br(),
-                html.Br(),
-                html.Br(),
-                html.Label('Try'),
-                html.Br(),
-                html.Br(),
-                html.Br(),
-                html.Br(),
-                html.Br(),
-                html.Label('Try'),
-                html.Br(),
-                html.Br(),
-                html.Br(),
-                html.Br(),
-                html.Br(),
-                html.Label('Try'),
-                html.Br(),
-                html.Br(),
-                html.Br(),
-                html.Br(),
-                html.Br(),
-                html.Label('Try'),
-                html.Label('Try'),
-                html.Br(),
-                html.Br(),
-                html.Br(),
-                html.Br(),
-                html.Br(),
-                html.Label('Try'),
-                html.Br(),
-                html.Br(),
-                html.Br(),
-                html.Br(),
-                html.Br(),
-                html.Label('Try'),
-                html.Br(),
-                html.Br(),
-                html.Br(),
-                html.Br(),
-                html.Br(),
-                html.Label('Try'),
+                html.Div([
+            'Who is GRID?'
+        ],
+            style={
+                'font-size':24,
+                'font-weight': 'bold'
+            }),
+        html.Div([
+            html.Div(['The Global Renewables Infrastructure Development (GRID) is a student organization at University of Washington with the goal of researching and developing renewable energy technologies. The main focus of GRID is to help undersized, climate frontline communities by providing the resources to employ renewable energy techniques with optimal design. Projects are off-grid and small-scale, yet contribute to equitable green solutions to energy demands. For more information on GRID and ongoing projects, check out the official site at ',  html.A("sites.uw.edu/grid/", href='https://sites.uw.edu/grid/', target="_blank"),], ),
+        ],
+                 style={
+                    'font-size': 20,
+                }),
+                ],
+                style={
+                    'float': 'left',
+                    'height': '35vh',
+                    'width': '56%',
+                    },
+                ),
+            dbc.Col([
+                html.Img(
+                src= '/assets/chelsea-WvusC5M-TM8-unsplash.jpg',
+                style={
+                    'width': '100%',
+                    'height': '40vh',
+                    'textAlign': 'center',
+                }
+                ),
+        # ),
+                ],
+                style={
+                    'float': "right",
+                    'height': '35vh',
+                    'width': '44%'
+                }
+                ),
+        ],
+                style={
+                    'height': '35vh',
+                }
+        ),
+        
+        # html.Center(
+        
+        
+        html.Br(),
+        html.Br(),
+        html.Br(),
+        html.Div([
+            'How it works?'
+        ],
+            style={
+                'font-size':24,
+                'font-weight': 'bold'
+                }),    
+        html.Div([
+            'The input box can be found at the bottom of this page. After you submit a location and load profile, you can run the program. The software pulls in irradiance data from The National Solar Radiation Database hosted by the National Renewable Energy Laboratory using the Physical Solar Model (PSM) v3. See the next section for detailed instructions on how to get started.'
+        ],
+                 style={
+                    'font-size': 20,
+                }),
+        html.Br(),
+        html.Div([
+            html.Div([
+            'Instructions'
+        ],
+            style={
+                'font-size':20,
+                'font-weight': 'bold'
+                }),    
+        html.Div([
+            'To run the program, first find the latitude and longitude in degrees of your desired location within the United States. The location must be within the US! You can use Google Maps to find the latitude and longitude of any location.'
+        ],
+                style={'padding-bottom': '10px',
+                       'font-size': 20,
+                       }),
+        html.Div([
+            'Next, upload your load profile by clicking the box labelled “Upload Load Profile”. Your load profile must be in csv format. We have provided a template of a load profile within the data directory in the SolarSizer github: ',
+            html.A("github.com/UW-Solar-GRID/SolarSizer/blob/main/doc/load_profile_template.csv", href='https://github.com/UW-Solar-GRID/SolarSizer/blob/main/doc/load_profile_template.csv', target="_blank"),
+            'You can refer to the load profile example within the data directory on how to fill out the template'
+        ],
+                style={'padding-bottom': '10px', 'font-size': 20,}),
+        html.Div([
+            'Once you have input your location and uploaded a load profile, press the RUN button to start the program. The solar array produced by the model will be displayed on the right. This contains the uptime percentage for each array specification.'
+        ],
+                style={'padding-bottom': '10px', 'font-size': 20,}),
+            ],
+                style={
+                    'padding-left': '30px'
+                }),
+        html.Br(),
+            ],
+            
+            style={
+                'padding-left': '30px',
+                'padding-right': '30px',
+                'padding-top': '30px',
+                'padding-bottom': '30px',
+            }),
         
         # Row for input and output
         dbc.Row([
             # Scrolling output, floating to the right
             dbc.Col(
                 children=[
-                    html.Label('Try'),
-                    html.Br(),
-                    html.Br(),
-                    html.Br(),
-                    html.Br(),
-                    html.Br(),
-                    html.Label('Try'),
-                    html.Br(),
-                    html.Br(),
-                    html.Br(),
-                    html.Br(),
-                    html.Br(),
-                    html.Label('Try'),
-                    html.Br(),
-                    html.Br(),
-                    html.Br(),
-                    html.Br(),
-                    html.Br(),
-                    html.Label('Try'),
-                    html.Br(),
-                    html.Br(),
-                    html.Br(),
-                    html.Br(),
-                    html.Br(),
-                    html.Label('Try'),
-                    html.Label('Try'),
-                    html.Br(),
-                    html.Br(),
-                    html.Br(),
-                    html.Br(),
-                    html.Br(),
-                    html.Label('Try'),
-                    html.Br(),
-                    html.Br(),
-                    html.Br(),
-                    html.Br(),
-                    html.Br(),
-                    html.Label('Try'),
-                    html.Br(),
-                    html.Br(),
-                    html.Br(),
-                    html.Br(),
-                    html.Br(),
-                    html.Label('Try'),
-                    dbc.Row([html.Div(id='model-status')]),
+                    html.Center([
+                        html.Div(['Your chart will show here'], style={'font-size': 24})
+                        ], 
+                        style={'vertical-align': 'top'}
+                    ),
+                    dbc.Row([
+                        html.Br(),
+                        html.Br(),
+                        dcc.Loading(
+                            id="loading-1",
+                            type="default",
+                            children=[html.Div(id='model-status')],
+                        ),
+                        ]),
                     ],
                 style={
                     'width': '64%',
                     'height': '68vh',
                     'float': 'right',
-                    'background-color': 'lightgreen',
+                    'background-color': 'silver',
                     'overflow': 'scroll',
-                    'borderRadius': '3px',
+                    'borderRadius': '8px',
                     'padding-top': '20px',
+                    'padding-left': '5px',
+                    'padding-right': '5px',
+                    'vertical-align': 'top',
                     },
             ),
             
@@ -197,19 +246,22 @@ app.layout = html.Div( children=[
             dbc.Card([
                 dbc.CardBody([
                     dbc.Row([
+                        html.Br(),
                         html.Div([
                         html.Div('Enter latitude and longitude below:', style={'fontSize': 24,}),
-                        html.Div('Note: chosen point must be in the United States', style={'color': 'gray', 'fontSize': 14, 'padding-bottom': '3px',}),
+                        html.Div('Note: chosen point must be in the United States', style={'color': 'black', 'fontSize': 14, 'padding-bottom': '3px',}),
                         
                         html.Label('Latitude (in degrees):'),
                         dcc.Input(id='lat', type='number'),
+                        html.Div(id=''),
                         html.Br(),
                         
                         html.Label('Longitude (in degrees):'),
                         dcc.Input(id='lon', type='number'),
+                        html.Div(id=''),
                         html.Br(), 
                     ],
-                    style={'width': '78%', 'display': 'inline-block'}
+                    style={'width': '86%', 'display': 'inline-block'}
                     ),
                     ]),
                     
@@ -221,7 +273,7 @@ app.layout = html.Div( children=[
                         dbc.Row(
                             [
                                 html.Center([
-                                    dbc.Row([html.Div('Note: load profile must be in csv format, see template in data directory', style={'color': 'gray', 'fontSize': 14}),]),
+                                    dbc.Row([html.Div('Note: load profile must be in csv format, see template in data directory', style={'color': 'black', 'fontSize': 14}),]),
                                     dcc.Upload(
                                         id='upload-data',
                                         children=[
@@ -262,12 +314,13 @@ app.layout = html.Div( children=[
                     dbc.Row([
                         html.Button('Run', id='btn-nclicks-1', n_clicks=0, 
                                     style={
+                                        'font-size': 30,
                                         'width': '80%',
                                         'height': '60px',
                                         'borderWidth': '2px',
-                                        'borderRadius': '5px',
+                                        'borderRadius': '20px',
                                         'textAlign': 'center',
-                                        'color': 'grey',
+                                        'color': 'black',
                                     },
                                     ),
                     ],
@@ -283,19 +336,22 @@ app.layout = html.Div( children=[
             ],
             style={
                 'display': 'inline-block',
-                'height': '54vh',
+                'height': '60vh',
                 'width': '35%',
                 'float': 'left',
                 'text-align': 'center',
                 'color':'black',
-                'background-color': 'thistle',
-                'borderRadius': '3px',
+                'background-color': 'cornflowerblue',
+                'borderRadius': '10px',
+                # 'opacity': '0.4',
                 },
             ),
         ],
         
         style={
-            'padding': '5px 5px',
+            'padding-top' : '45px',
+            'padding-left': '3px',
+            'padding-right': '5px',
         }
         ),
         ],
@@ -304,7 +360,7 @@ app.layout = html.Div( children=[
             'width': '100%',
             'height': '70vh',
             'overflow': 'scroll',
-            'background-color': 'yellow',
+            'background-color': 'white',
         }
         ),
     ],
@@ -312,8 +368,9 @@ app.layout = html.Div( children=[
             'width': '100%',
             'height': '70vh',
             'overflow': 'scroll',
-            'background-color': 'gray',
-        }        
+            'borderRadius': '7px',
+            'background-color': 'white',
+        } ,       
     
     ),
 
@@ -342,7 +399,8 @@ def update_output(lat, lon):
     """
     Updates output with input values run through FakeSAM model.
     """
-
+    
+    time.sleep(3)
     if lat is not None and lon is not None:
         global_lat = lat
         global_lon = lon
@@ -353,6 +411,7 @@ def update_output(lat, lon):
 
     else:
         pass
+
 
 
 @app.callback(Output('output-data-upload', 'children'),
@@ -401,25 +460,24 @@ def displayClick(btn1):
         test_df = model_output
         print(model_output)
         print('test_df++++++++', test_df)
-        
-        msg = 'Model running'
-        return html.Div(msg)
     
-    elif not test_df.empty:
+    if not test_df.empty:
         msg = 'Model finished running, result below:'
-        return [html.Div(msg), 
+        msg2 = 'Chart is loading'
+        return [
+            html.Div(msg), 
             
             html.Div([
                 dash_table.DataTable(
                     data=test_df.to_dict('records'),
-                    columns=[{'name': i, 'id': i} for i in test_df.columns]
+                    columns=[{'name': i, 'id': i} for i in test_df.columns],
                 ),
                 html.Hr(),])
                 ]
-    else:
-        msg = "placeholder"
-        # msg = 'Click button to run model once the lat and lon are inputted and a .csv load profile is uploaded'
-        return html.Div(msg)
+    # else:
+    #     msg = "placeholder"
+    #     # msg = 'Click button to run model once the lat and lon are inputted and a .csv load profile is uploaded'
+    #     return html.Div(msg)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
